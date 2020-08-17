219397a453ee8ba93a2777f236474ffc207e3380
Paulo Henrique Braga <paulo@someletras.com.br>
2020-08-17 01:32:32 -0300

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
+import 'package:app_constroca/providers/Users.dart';
 import 'package:flutter/material.dart';
 import 'package:app_constroca/splash.dart';
+import 'package:provider/provider.dart';
 
 import 'constants.dart';
 
 void main() => runApp(Constroca());
 
 class Constroca extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: APP_NAME,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => Users())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: APP_NAME,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        //routes: ,
+        home: Splash(),
       ),
-      home: Splash(),
     );
   }
 }
