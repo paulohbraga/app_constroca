ff6a9d3f4c9c5400d43d0841bc58dd6f58dd4471
Paulo Henrique Braga <paulo@someletras.com.br>
2020-08-24 19:21:05 -0300

    opt imports

--- lib/doacao.dart
+++ lib/doacao.dart
@@ -1,16 +1,16 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:app_constroca/perfil.dart';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_open_whatsapp/flutter_open_whatsapp.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
 import 'package:transparent_image/transparent_image.dart';
+
 import 'appdata.dart';
 import 'constants.dart';
-import 'package:flutter_open_whatsapp/flutter_open_whatsapp.dart';
 import 'detalhaProduto.dart';
-import 'perfil.dart';
 
 Future<List<Produto>> fetchProdutos(http.Client client) async {
   final response = await client.get('http://192.168.15.10/api/produto/readDonation.php');
