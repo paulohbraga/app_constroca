728c974135c8b1509f1e0afed3411accbb96704e
Paulo Henrique Braga <paulo@someletras.com.br>
2020-09-04 18:02:54 -0300

    products using new API - Working

--- lib/produtos.dart
+++ lib/produtos.dart
@@ -11,63 +11,58 @@ import 'package:transparent_image/transparent_image.dart';
 import 'appdata.dart';
 import 'constants.dart';
 import 'detalhaProduto.dart';
+import 'models/Usuario.dart';
 
 Future<List<Produto>> fetchProdutos(http.Client client) async {
-  final response = await client.get('http://192.168.15.10/api/produto/read.php');
+  final response = await client.get('http://localhost:8080/produtos/');
 
   // Use the compute function to run parsePhotos in a separate isolate
   return compute(parseProdutos, response.body);
 }
 
 // A function that will convert a response body into a List<Photo>
-List<Produto> parseProdutos(String responseBody) {
-  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+List<Produto> parseProdutos(String str) => List<Produto>.from(json.decode(str).map((x) => Produto.fromJson(x)));
 
-  return parsed.map<Produto>((json) => Produto.fromJson(json)).toList();
-}
+String produtoToJson(List<Produto> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Produto {
-  final String id_produto;
-  final String fk_id_usuario;
-  final String nome_usuario;
-  final String nome_produto;
-  final String telefone;
-  final String email;
-  final String descricao_produto;
-  final String imagem;
-  final String tipo;
-  final String avatar;
+  Produto({
+    this.id,
+    this.nomeProduto,
+    this.descricao,
+    this.status,
+    this.tipo,
+    this.imagem,
+    this.usuario,
+  });
 
-  Produto(
-      {this.id_produto,
-      this.fk_id_usuario,
-      this.nome_usuario,
-      this.nome_produto,
-      this.telefone,
-      this.email,
-      this.descricao_produto,
-      this.imagem,
-      this.avatar,
-      this.tipo});
+  String id;
+  String nomeProduto;
+  String descricao;
+  String status;
+  String tipo;
+  String imagem;
+  Usuario usuario;
 
-  factory Produto.fromJson(Map<String, dynamic> json) {
-    return Produto(
-      id_produto: json['id_produto'] as String,
-      fk_id_usuario: json['fk_id_usuario'] as String,
-      nome_usuario: json['nome_usuario'] as String,
-      nome_produto: json['nome_produto'] as String,
-      telefone: json['telefone'] as String,
-      email: json['email'] as String,
-      descricao_produto: json['descricao_produto'] as String,
-      imagem: json['imagem'] as String,
-      avatar: json['avatar'] as String,
-      tipo: json['tipo'] as String,
-      //email
-      //telefone
-      //cidade
-      // avatar
-    );
-  }
+  factory Produto.fromJson(Map<String, dynamic> json) => Produto(
+        id: json["id"],
+        nomeProduto: json["nome_produto"],
+        descricao: json["descricao_produto"],
+        status: json["status"],
+        tipo: json["tipo"],
+        imagem: json["imagem"],
+        usuario: Usuario.fromJson(json["usuario"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "nome_produto": nomeProduto,
+        "descricao_produto": descricao,
+        "status": status,
+        "tipo": tipo,
+        "imagem": imagem,
+        "usuario": usuario.toJson(),
+      };
 }
 
 class MyApp extends StatelessWidget {
@@ -165,19 +160,15 @@ class ProdutosList extends StatelessWidget {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => MyHomePageDetail(
-                                produtos[index].descricao_produto,
-                                produtos[index].id_produto,
-                                produtos[index].nome_produto,
-                                produtos[index].imagem,
-                                produtos[index].avatar))),
-                    appData.id_produto = produtos[index].id_produto,
-                    appData.name_produto = produtos[index].nome_produto,
+                            builder: (context) => MyHomePageDetail(produtos[index].descricao, produtos[index].id,
+                                produtos[index].nomeProduto, produtos[index].imagem, produtos[index].usuario.avatar))),
+                    appData.id_produto = produtos[index].id,
+                    appData.name_produto = produtos[index].nomeProduto,
                     appData.img_produto = produtos[index].imagem,
-                    appData.descricao_produto = produtos[index].descricao_produto,
-                    appData.email_client = produtos[index].email,
-                    appData.avatar_client = produtos[index].avatar,
-                    appData.telefone_client = produtos[index].telefone
+                    appData.descricao_produto = produtos[index].descricao,
+                    appData.email_client = produtos[index].usuario.email,
+                    appData.avatar_client = produtos[index].usuario.avatar,
+                    appData.telefone_client = produtos[index].usuario.telefone.toString()
                   },
                   child: Stack(
                     children: <Widget>[
@@ -185,7 +176,7 @@ class ProdutosList extends StatelessWidget {
                         semanticContainer: true,
                         clipBehavior: Clip.antiAliasWithSaveLayer,
                         child: Hero(
-                          tag: produtos[index].id_produto,
+                          tag: produtos[index].id,
                           child: FadeInImage.memoryNetwork(
                             fadeInDuration: const Duration(milliseconds: 400),
                             image: "http://192.168.15.10/api/produto/imagens/" + produtos[index].imagem + "",
@@ -216,7 +207,7 @@ class ProdutosList extends StatelessWidget {
                             ),
                             child: Padding(
                               padding: const EdgeInsets.all(15),
-                              child: Text(produtos[index].nome_produto,
+                              child: Text(produtos[index].nomeProduto,
                                   textAlign: TextAlign.end,
                                   style: TextStyle(
                                       fontFamily: 'Raleway',
